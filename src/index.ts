c31fdd001a6285469a31d46eccf80f3bea05f146
Moghees <electricinfodata@gmail.com>
2024-11-22 11:06:09 -0700

    Refactor import of d3-node for dynamic loading
    Changed the import of `d3-node` to dynamic loading within the handler function. This ensures `d3-node` is only imported as needed, potentially improving startup performance.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 // @ts-ignore
-import { D3Node } from 'd3-node' // const D3Node = require('d3-node')
 export const handler = async ( event: APIGatewayProxyEvent ): Promise<APIGatewayProxyResult> => {
   try {
+    const { D3Node } = await import('d3-node');
     // Your logic here
     const d3n = new D3Node()      // initializes D3 with container element
     d3n.createSVG(10,20).append('g') // create SVG w/ 'g' tag and width/height
