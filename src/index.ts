41113b7859d31c076a55a4fca767d2202601b320
Moghees <electricinfodata@gmail.com>
2024-11-22 10:59:30 -0700

    Update tsconfig.json for improved TypeScript compilation
    Switched module system to CommonJS and adjusted module resolution to Node. Enabled preserveConstEnums and disabled source maps for a cleaner build. Ensured consistency and interop settings remain intact.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-
+// @ts-ignore
+import { D3Node } from 'd3-node' // const D3Node = require('d3-node')
 export const handler = async ( event: APIGatewayProxyEvent ): Promise<APIGatewayProxyResult> => {
   try {
     // Your logic here
-    const message = 'Hello, world!';
+    const d3n = new D3Node()      // initializes D3 with container element
+    d3n.createSVG(10,20).append('g') // create SVG w/ 'g' tag and width/height
+    
+    const message = d3n.svgString() // output: <svg width=10 height=20 xmlns="http://www.w3.org/2000/svg"><g></g></svg>;
     
     return {
       statusCode: 200,
